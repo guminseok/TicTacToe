f2a5b6d44d47fe53eeb6b01cd51ad211be79c2a5
guminseok <gumajuc@gmail.com>
2024-01-23 16:14:12 +0900

    勝利条件追加

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -2,6 +2,17 @@ import React, { useState } from 'react';
 import './TicTacToe.css';
 
 const TicTacToe = () => {
+  //勝利条件追加
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,5,6],
+  ];
 
   const winner = null;
 
