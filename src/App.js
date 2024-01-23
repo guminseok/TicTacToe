ac65c747c22cc539ab0cbb2bc74de08eacfe225d
guminseok <gumajuc@gmail.com>
2024-01-23 12:32:33 +0900

    App.jsにcssファイル追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import TicTacToe from "./components/TicTacToe";
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <TicTacToe />
+    <div className="game">
+      <div className="game-board">
+        <TicTacToe />
+      </div>
     </div>
   );
 }
