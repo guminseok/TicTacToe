1ef434bc8a80d3eae1dc94893bc97e2faaa3cb30
guminseok <gumajuc@gmail.com>
2024-01-23 13:58:46 +0900

    Next player文言追加

---
+++ src/components/TicTacToe.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './TicTacToe.css';
+
+const TicTacToe = () => {
+  return(
+    <div>
+      <div className="status">Next player: </div>
+    </div>
+  );
+}
+
+export default TicTacToe;
