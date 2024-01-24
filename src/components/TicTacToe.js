26f4f5cbb5b16068fb3de3f8de0da8868f6d3828
guminseok <gumajuc@gmail.com>
2024-01-24 14:14:55 +0900

    tictactoc完成

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -14,18 +14,25 @@ const TicTacToe = () => {
       [0, 4, 8],
       [2, 4, 6],
     ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
   const winner = calculateWinner(squares);
 
   const getStatus = () => {
-    if(winner) {
-      return "X"
-    } else if (winner == "a"){
-      return "Y"
+    if (winner) {
+      return `Winner: ${winner}`;
+    } else if (squares.every((a) => a)) {
+      return 'Draw!';
     } else {
-      return "Next player: ";
+      return `Next player: ${xIsNext ? 'X' : 'O'}`;
     }
   };
 
