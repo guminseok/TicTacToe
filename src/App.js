757cc4df52e0683e7255a9f9763375dda7c2f00f
guminseok <gumajuc@gmail.com>
2024-01-26 16:42:16 +0900

    css修正

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import TicTacToe from "./components/TicTacToe";
 import TicTacToeTest from "./components/TicTacToeTest";
 import TicTacToeDev from "./components/TicTacToeDev";
 import TpyeReducer from "./components/TpyeReducer.tsx";
+import TpyeContext from "./components/TpyeContext.tsx";
 import './App.css';
 
 function App() {
   return (
     <div className="game">
+      <div className="margin-b"><TpyeContext /></div>
       <div className="game-board">
         <TicTacToe />
-        <div>Test</div>
+        <div className="margin-b"></div>
+        Test
         <TicTacToeTest />
       </div>
       <div>
+        <div className="margin-b"></div>
         <div>勉強するサイトで学習</div>
         <TicTacToeDev />
       </div>
+      <div className="margin-b"></div>
       <div><TpyeReducer /></div>
     </div>
   );
