da4aba303d8224128578a8f3937e61cfe98a0147
guminseok <gumajuc@gmail.com>
2024-01-26 17:52:51 +0900

    条件付きレンダー

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,27 +1,31 @@
 import React from "react";
 
-const person = {
-  name: 'Gregorio Y. Zara',
-  theme: {
-    backgroundColor: 'black',
-    color: 'pink'
-  }
-};
+function Item({ name, isPacked }) {
+  return (
+    <li className="item">
+      {name} {isPacked && '✔'}
+    </li>
+  );
+}
 
 export default function Gallery() {
   return (
-    <div style={person.theme}>
-      <h1>{person.name}'s Todos</h1>
-      <img
-        className="avatar"
-        src="https://i.imgur.com/7vQD0fPs.jpg"
-        alt="Gregorio Y. Zara"
-      />
+    <section>
+      <h1>Sally Ride's Packing List</h1>
       <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
+        <Item
+          isPacked={true}
+          name="Space suit"
+        />
+        <Item
+          isPacked={true}
+          name="Helmet with a golden leaf"
+        />
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
+        />
       </ul>
-    </div>
+    </section>
   );
 }
