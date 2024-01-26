55ec20438579e22a681810c6e86945fa3c30e16d
guminseok <gumajuc@gmail.com>
2024-01-26 16:20:19 +0900

    コメントアウト

--- src/components/TpyeSc.tsx
+++ src/components/TpyeSc.tsx
@@ -11,17 +11,18 @@ type CounterAction =
 const initialState: State = { count: 0 };
 
 function stateReducer(state: State, action: CounterAction): State {
-  switch (action.type) {
+  switch (action.type) {// type: "setCount"
     case "reset":
       return initialState;
     case "setCount":
-      return { ...state, count: action.value };
+      return { ...state, count: action.value };//value: state.count + 5
     default:
       throw new Error("Unknown action");
   }
 }
 
 export default function TpyeSc() {
+  //initialState 初期値、stateReducerどういうふうにアップデートされるか表示
   const [state, dispatch] = useReducer(stateReducer, initialState);
 
   const addFive = () => dispatch({ type: "setCount", value: state.count + 5 });
