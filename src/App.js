9823dc23560ac8df46ffda371779f4049ae1448a
guminseok <gumajuc@gmail.com>
2024-01-26 17:04:23 +0900

    Callback

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import TicTacToeDev from "./components/TicTacToeDev";
 import TpyeReducer from "./components/TpyeReducer.tsx";
 import TpyeContext from "./components/TpyeContext.tsx";
 import TpyeMemo from "./components/TpyeMemo.tsx";
+import TpyeCallback from "./components/TpyeCallback.tsx";
 import './App.css';
 
 function App() {
   return (
     <div className="game">
+      <div className="margin-b"><TpyeCallback /></div>
       <div className="margin-b"><TpyeMemo /></div>
       <div className="margin-b"><TpyeContext /></div>
       <div className="game-board">
